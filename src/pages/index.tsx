2caca750c9eafde03c65da763b683983f3a0eda7
GABRIEL PEDRO RAMALHO <biel-pedro@hotmail.com>
2023-11-30 20:00:37 -0300

    Add title and meta tags to pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { GetStaticProps } from "next";
 import Link from "next/link";
+import Head from "next/head";
 
 import { useKeenSlider } from 'keen-slider/react'
 import 'keen-slider/keen-slider.min.css';
@@ -32,25 +33,32 @@ export default function Home({ products }: HomeProps) {
   })
 
   return (
-    <HomeContainer ref={sliderRef} className="keen-slider">
-      {
-        products.map(product => {
-          return(
-            <Link key={product.id} href={`product/${product.id}`} prefetch={false}>
-              <Product  className="keen-slider__slide" >
-                <Image src={product.imageUrl} width={520} height={480} alt=""/>
-                <footer>
-                  <strong>{product.name}</strong>
-                  <span>{product.price}</span>
-                </footer>
-              </Product>
-            </Link>
 
-          )
-        })
-      }
+    <>
+    <Head>
+      <title>Home | Ignite Shop</title>
+    </Head>
+      <HomeContainer ref={sliderRef} className="keen-slider">
+        {
+          products.map(product => {
+            return(
+              <Link key={product.id} href={`product/${product.id}`} prefetch={false}>
+                <Product  className="keen-slider__slide" >
+                  <Image src={product.imageUrl} width={520} height={480} alt=""/>
+                  <footer>
+                    <strong>{product.name}</strong>
+                    <span>{product.price}</span>
+                  </footer>
+                </Product>
+              </Link>
 
-    </HomeContainer>
+            )
+          })
+        }
+
+      </HomeContainer>
+    </>
+
   )
 }
 
